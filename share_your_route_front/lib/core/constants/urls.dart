7233e52fe4d215e66ab69f17cfd69b3b471edd69
Roberto Patiño <rapatino@espol.edu.ec>
2024-08-12 23:57:08 -0500

    Removed background on settings image

--- share_your_route_front/lib/core/constants/urls.dart
+++ share_your_route_front/lib/core/constants/urls.dart
@@ -3,7 +3,7 @@ const String logoURL = "asset/images/logo.png";
 const String accessibilityScreenLogoURL =
     "asset/images/accessibility_screen_img.png";
 const String helpScreenLogoURL = "asset/images/help_screen_img.webp";
-const String settingsScreenLogoURL = "asset/images/settings_screen_img.jpg";
+const String settingsScreenLogoURL = "asset/images/settings_screen_img.png";
 const String stockProfilePictureURL = "asset/images/stock_user_image.jpg";
 const String stockBackgroundPictureURL =
     "asset/images/stock_background_image.jpeg";
