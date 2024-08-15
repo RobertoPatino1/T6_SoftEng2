1e761e8b648c8d57591bac76eb84a054ca240b02
Stefano Suarez Herrera <stesherr@espol.edu.ec>
2024-08-15 01:50:10 -0500

    Structure Notification Page

---
+++ share_your_route_front/lib/modules/notification/presenters/notification_page.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class NotificationPage extends StatefulWidget {
+  @override
+  _NotificationPageState createState() => _NotificationPageState();
+}
+
+class _NotificationPageState extends State<NotificationPage> {
+  int currentPageIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
